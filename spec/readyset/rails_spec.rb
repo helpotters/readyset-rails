e2b3a03507d8d6330b3caac71f56faba5b7fe93c
Paul Lemus <paullemus@protonmail.com>
2023-10-23 12:59:30 -0700

    initial commit

---
+++ spec/readyset/rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Readyset::Rails do
+  it "has a version number" do
+    expect(Readyset::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
